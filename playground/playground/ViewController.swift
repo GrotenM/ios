f48499985324f3db640f700fa1d1db23ce482be2
GrotenM <m.groten@yahoo.de>
2017-06-18 23:52:57 +0200

    XCode Project erstellt

---
+++ playground/playground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  playground
+//
+//  Created by Maik Groten on 18.06.17.
+//  Copyright © 2017 Maik Groten. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
